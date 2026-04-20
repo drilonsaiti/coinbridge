fdcd57a5d1204092772768a9535ded5d4dc81e44
drilonsaiti <drilon-saiti@hotmail.com>
2026-04-20 17:27:28 +0200

    implement if trending is neutral

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { clsx, type ClassValue } from 'clsx';
-import { Time } from 'lightweight-charts';
-import { twMerge } from 'tailwind-merge';
+import {clsx, type ClassValue} from 'clsx';
+import {Time} from 'lightweight-charts';
+import {twMerge} from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -40,11 +40,12 @@ export function formatPercentage(change: number | null | undefined): string {
 
 export function trendingClasses(value: number) {
     const isTrendingUp = value > 0;
+    const isNeutral = value === 0;
 
     return {
-        textClass: isTrendingUp ? 'text-green-400' : 'text-red-400',
-        bgClass: isTrendingUp ? 'bg-green-500/10' : 'bg-red-500/10',
-        iconClass: isTrendingUp ? 'icon-up' : 'icon-down',
+        textClass: isNeutral ? 'text-gray-400' : isTrendingUp ? 'text-green-400' : 'text-red-400',
+        bgClass: isNeutral ? 'bg-gray-500/10' : isTrendingUp ? 'bg-green-500/10' : 'bg-red-500/10',
+        iconClass: isNeutral ? 'icon-neutral' : isTrendingUp ? 'icon-up' : 'icon-down',
     };
 }
 
