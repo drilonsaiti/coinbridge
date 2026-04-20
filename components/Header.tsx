ecb556ac07e1360f576db89b552937698ab9c140
Drilon <50522333+drilonsaiti@users.noreply.github.com>
2026-04-20 11:57:04 +0200

    Update components/Header.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,14 +12,32 @@ const Header = () => {
                 </Link>
 
                 <nav>
+'use client'
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import {cn} from "@/lib/utils";
+
+const Header = () => {
+    const pathname = usePathname()
+
+    return (
+        <header>
+            <div className="main-container inner">
+                <nav>
                     <Link href={"/"} className={cn('nav-link',{
-                        'is-active': location.pathname === '/',
+                        'is-active': pathname === '/',
                         'is-home': true
                     })}>Home</Link>
                     <p>Search Modal</p>
                     <Link href={"/coins"} className={cn('nav-link',{
-                        'is-active': location.pathname === '/coins'
+                        'is-active': pathname === '/coins'
                     })}>All Coins</Link>
+                </nav>
+            </div>
+        </header>
+    )
+}
                 </nav>
             </div>
         </header>
